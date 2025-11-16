14d87315d19de2980a414684775a532b9dabbf7f
king-hypertext <oseikin7@gmail.com>
2025-11-16 18:01:20 +0000

    first deploy commit

---
+++ dist/assets/RegisterView-DbMeuT0G.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index-CE29yLrG.js";const r={};function t(c,s){return" register "}const n=e(r,[["render",t]]);export{n as default};
--- src/views/auth/RegisterView.vue
+++
@@ -1,8 +0,0 @@
-<script setup lang="ts">
-</script>
-
-<template>
-  register
-</template>
-
-<style scoped></style>
